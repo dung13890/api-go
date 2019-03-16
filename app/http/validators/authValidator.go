e20b6f528c0bd65efbdc335f8c323cb26a5bf8db
Mr. D <dung13890@gmail.com>
2019-03-16 21:51:24 +0700

    user store & delete

--- app/http/validators/authValidator.go
+++ app/http/validators/authValidator.go
@@ -1,6 +1,8 @@
 package validators
 
 import (
+	"fmt"
+
 	"github.com/dung13890/api-go/models"
 	validator "gopkg.in/go-playground/validator.v9"
 )
@@ -15,4 +17,31 @@ func Login(m *models.User) []string {
 	renderValidations(&validations, "Password", errPass)
 
 	return validations
+}
+
+func UserStore(m *models.User) []string {
+	validate := validator.New()
+	validate.RegisterValidation("unique", validUnique)
+	validations := []string{}
+	err := validate.Struct(m)
+
+	if err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			validations = append(validations, err.Error())
+			return validations
+		}
+
+		for _, e := range err.(validator.ValidationErrors) {
+			validations = append(validations, fmt.Sprintf("%s: Rule %s is invalid", e.Field(), e.Tag()))
+		}
+	}
+
+	return validations
+}
+
+func validUnique(fl validator.FieldLevel) bool {
+	currentField, _, _ := fl.GetStructFieldOK()
+	value := fl.Field().String() // value
+	fmt.Println(currentField, value)
+	return false
 }
